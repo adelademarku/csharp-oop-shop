3b4133703233d59cb141be863b4f2f975ba5d799
adelademarku <adelademarku@email.com>
2022-11-29 15:17:34 +0100

    aggionramento

--- CSharpShop/Program.cs
+++ CSharpShop/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using CSharpShop;
+
+Prodotto primoProdotto = new Prodotto("libro", "bambini", 15, 4);
+
+
